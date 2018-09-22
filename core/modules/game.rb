f95ff087fc9c6ab17059d47d382c195b8da2d417
Alastar Corr <house.labs@gmail.com>
2018-09-22 17:03:15 +0600

    Sum Points

--- core/modules/game.rb
+++ core/modules/game.rb
@@ -9,14 +9,27 @@ module Game
   end
 
   def starting_cards
-    @player.cards << @deck[0..1]
-    @dealer.cards << @deck[2..3]
+    @player.cards << @deck[0]
+    @player.cards << @deck[1]
+    @dealer.cards << @deck[2]
+    @dealer.cards << @deck[3]
+  end
+
+  def sum_points
+    sum = 0
+    @player.cards.each do |card|
+      sum += Integer(card[0]) if card[0].to_i > 2
+      sum += 1 if card[0] == 'A'
+      sum += 10 if card[0].to_i.zero?
+    end
+    @sum_points = sum
   end
 
   def give_cards
     puts 'Раздача карт'
     puts "Карты #{@player.name}"
     puts @player.cards
+    puts "Сумма очков: #{sum_points}"
     puts "Карты #{@dealer.name}"
     2.times { puts '*' }
   end
